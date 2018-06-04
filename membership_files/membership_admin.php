b93e2be8aa5f356c9abe0961ae5d468918c01959
scottyzsigs <staylor_au@hotmail.com>
2018-06-04 12:49:20 +1000

    webmaster only, remove debug

--- membership_files/membership_admin.php
+++ membership_files/membership_admin.php
@@ -6,7 +6,15 @@ require('/home/canber10/public_html/cbadmin/web_incs/web_db.inc');
 
 require('/home/canber10/public_html/cbadmin/web_incs/forum_auth.php');
 $admin = false;
-$debug = false;
+$admin_type = '';
+$debug_mode = false;
+
+if($debug_mode)
+{
+	error_reporting(E_ALL); 
+	ini_set('display_errors', 1);
+}
+$access_msg = '<h1>Access Denied</h2><p>Please contact webmaster@canberrabrewers.com.au for access.</p>';
 if($user_id == '' || $user_id == '1')
 {
 	$returnpage = '/membership_files/membership_admin.php';
@@ -20,12 +28,13 @@ else
 	while ($urow = mysqli_fetch_array($u_result))
 	{
 		$admin = true;
+		$admin_type = $urow['admin_type'];
 	}
 	
 }
 if($admin != true)
 {
-	echo '<h1>Access Denied</h2><p>Please contact webmaster@canberrabrewers.com.au for access.</p>';
+	echo $access_msg;
 }
 else
 {
@@ -43,22 +52,35 @@ p, td, div {font-family:Arial, "Helvetica Neue", Helvetica, sans-serif; font-siz
 .form-row input[type="text"] {min-width:40%;margin-right:10px;}
 .form-help {font-size: 0.8em; color:gray;}
 .required, .error {color:red;}
+table {width:80%;border:1px solid #ccc;}
+td {padding:1rem;}
+th {background: #ccc;width:20%;}
+tr:nth-child(even) {background: #DAE4F0}
+tr:nth-child(odd) {background: #FFF}
 </style>
 </head>
 <body>
 <h1>Canberra Brewers Member Admin</h1>
 <ul>
-<li><a href="/member_admin/?emailform">Send email to members</a></li>
+<li><a href="membership_admin.php?action=emailform">Send email to members</a></li>
+<?php
+if($admin_type == 'members') {
+?>
+<li><a href="membership_admin.php?action=memberlist">Manage Membership</a></li>
+<?php
+}
+?>
 </ul>
 <?php
 	// set up variables
 	$action = !empty($_GET['action']) ? $_GET['action'] : ''; // was action sent?
+	$emailaction = !empty($_GET['emailaction']) ? $_GET['emailaction'] : ''; // was action sent?
 	$formaction = !empty($_POST['formaction']) ? $_POST['formaction'] : ''; // was form action sent?
 	$message = '';
 	$sent_to = '';
 	// do the actions for forms
 	switch ($formaction) {
-		case 'sendemail':
+		case 'Send email to members':
 			$emailsubject = !empty($_POST['emailsubject']) ? $_POST['emailsubject'] : 'Message from Canberra Brewers';
 			$msg = '<style type="text/css">body, h1, h2, h3  {font-family:Arial, "Helvetica Neue", Helvetica, sans-serif; } p, td, div {font-family:Arial, "Helvetica Neue", Helvetica, sans-serif; }</style>';
 			$msg .= !empty($_POST['emailmsg']) ? $_POST['emailmsg'] : '';
@@ -83,7 +105,7 @@ p, td, div {font-family:Arial, "Helvetica Neue", Helvetica, sans-serif; font-siz
 				{
 					// v1 limited to active members from the forum
 					$sql = '';
-					if($debug)
+					if($debug_mode)
 					{
 						$sql = "SELECT user_email FROM forumv3_users WHERE username = 'ScottyT'";
 					}
@@ -100,35 +122,53 @@ p, td, div {font-family:Arial, "Helvetica Neue", Helvetica, sans-serif; font-siz
 					}
 				}
 			}
+			$action = 'emailform';
 			break;
 		case 'updatemember':
 			echo "update the member";
 			break;
 }
-if($sent_to != '')
+switch ($action) {
+	case 'emailform':
+	
+	if($sent_to != '')
 {
 echo '<p>'.$sent_to.'</p>';
 }
-if($debug) { echo 'debug mode on';}
+
+if($emailaction == '')
+{
+	$mailfval = "webmaster"; // bug in mail can't set to this, talk to hosting
+	$helpmsg = "You are sending to all active members, this function is for sending monthly reports or other important messages.";
+}
+else
+{
+	$year = !empty($_GET['year']) ? $_GET['year'] : '';
+	$mailfval = "webmaster";
+	$helpmsg = "You are sending to all unpaid members for the year ". $year .", this function is for sending reminders to members who have not paid membership fees.";
+}
+if($debug_mode) { $helpmsg .= '<br /><strong>debug mode on - do not use - contact webmaster@canberrabrewers.com.au</strong>';}
 ?>
 
 <script src="https://cdn.ckeditor.com/4.7.1/standard/ckeditor.js"></script>
 <h2>Send email to members</h2>
+<p><?php echo $helpmsg ?></p>
 <form method="post" novalidate="novalidate" id="emailform">
-<input type="hidden" name="formaction" value="sendemail" />
 <div class="form-row">
 	<div class="form-label"><label for="emailsubject">Email subject</label> <span class="required">*</span></div>
 	<input type="text" id="emailsubject" name="emailsubject" required />
 </div>
 <div class="form-row">
 	<div class="form-label"><label for="emailfrom">Email from</label> <span class="required">*</span></div>
-	<input type="text" id="emailfrom" name="emailfrom" value="webmaster" required />@canberrabrewers.com.au
+	<input type="text" id="emailfrom" name="emailfrom" value="<?php echo $mailfval ?>" required />@canberrabrewers.com.au
 </div>
+<?php if($emailaction == '') { ?>
 <div class="form-row">
 	<div class="form-label"><label for="emailto">Test email to</label></div>
 	<input type="text" id="emailto" name="emailto" />
 	<p class="form-help">Leave this blank to email all members otherwise use a single email address to test</p>
 </div>
+<?php } ?>
 <div class="form-row">
 <div class="form-label"><label for="emailmsg">Email body</label> <span class="required">*</span></div>
 <textarea name="emailmsg" required><?php echo htmlentities($msg); ?></textarea>
@@ -136,7 +176,9 @@ if($debug) { echo 'debug mode on';}
 	CKEDITOR.replace( 'emailmsg' );
 </script>
 </div>
-<input type="submit" value="Send email to members" />
+<?php if($emailaction == '') { ?>
+<input type="submit" name="formaction" value="Send email to members" />
+<?php } ?>
 </form>
 <script>
 jQuery.noConflict();
@@ -146,6 +188,56 @@ jQuery(document).ready(function ($) {
 });
 </script>
 <?php
+// end email form
+	break;
+	case 'memberlist':
+	if($admin_type != 'members')
+	{
+		echo $access_msg;
+	}
+	else
+	{
+	$thisyear = date("Y");
+	for ($x = $thisyear; $x >= 2017; $x--) {
+    
+	$qdate = $x .'-01-01 00:00:00';
+	$qedate = $x .'-12-31 23:59:59';	
+	$memberlistsql = "SELECT member_firstname, member_surname, member_email, cb_membership.member_id, transaction_code, transaction_date FROM cb_membership LEFT JOIN cb_membership_transactions ON cb_membership_transactions.member_id = cb_membership.member_id WHERE (DATE(`transaction_date`) BETWEEN '".$qdate."' AND '".$qedate."') ORDER BY member_firstname, member_surname"; 
+	$member_result=$mysqli->query($memberlistsql);
+	$num_rows = mysqli_num_rows($member_result);
+//echo $memberlistsql;
+	$tbl = "<table><thead><tr><th>Name</th><th>Email</th><th>Paid date</th><th>Transaction ID</th></tr></thead><tbody>%s</tbody></table>";
+?>
+
+<?php	
+	while ($row = mysqli_fetch_array($member_result))
+	{
+		$members .= '<tr><td>'.$row['member_firstname'].' '. $row['member_surname']. '</td><td>'. $row['member_email'] .'</td><td>'. $row['transaction_date'] .'</td><td>'. $row['transaction_code'] .'</td></tr>';
+	}
+	$tbldata = sprintf($tbl,$members);
+	echo '<h2>'.$x.' Paid Members ('.$num_rows.')</h2>'.$tbldata;
+	$members = '';
+	
+	$memberlistsql = "SELECT member_firstname, member_surname, member_email, cb_membership.member_id FROM cb_membership LEFT JOIN forumv3_users ON username = member_forum_name WHERE (user_type = 0 or user_type = 3) AND NOT EXISTS (SELECT * FROM cb_membership_transactions WHERE cb_membership_transactions.member_id = cb_membership.member_id AND (DATE(`transaction_date`) BETWEEN '".$qdate."' AND '".$qedate."')) ORDER BY member_firstname, member_surname"; 
+	//echo $memberlistsql;
+	$member_result=$mysqli->query($memberlistsql);
+	$num_rows = mysqli_num_rows($member_result);
+	while ($row = mysqli_fetch_array($member_result))
+	{
+		$members .= '<tr><td>'.$row['member_firstname'].' '. $row['member_surname']. '</td><td>'. $row['member_email'] .'</td><td>Unpaid for '. $x .'</td><td></td></tr>';
+	}
+	$tbldata = sprintf($tbl,$members);
+	echo '<h2>'.$x.' Unpaid Members ('.$num_rows.') | <a href="membership_admin.php?action=emailform&emailaction=members&year='.$x.'">Email unpaid '.$x.' members</a></h2>'.$tbldata;
+	$members = '';	
+	// end for
+	}
+	// end admin type check
+	}
+	break;
+
+}
+
+
 // end admin check and page
 }
 ?>
